91d36f3f9665975cbb731bd40875048639a3e958
Gregory Dechant <dechantg@gmail.com>
2023-10-19 03:27:43 +0000

    fixed app for SQL injection vulnerability

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,22 +10,22 @@ const pool = new Pool({
 });
 
 
+const cohortName = process.argv[2];
 
+const values = [`%${cohortName}%`];
 
-pool.query(`
+const queryString = `
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
+`;
 
-  SELECT teachers.name AS teacher, cohorts.name AS cohort
-  FROM assistance_requests
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
-  GROUP BY teachers.name, cohorts.name
-  ORDER BY teachers.name;`
-  
-  
-)
-
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(result => {
     console.log(`${result.cohort}: ${result.teacher}`);
